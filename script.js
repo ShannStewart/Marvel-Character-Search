f2086834c767edc0c3a67e1a3e8762344190dc1a
ShannStewart <Zackry.Broodie.Stewart@gmail.com>
2020-02-23 18:25:12 -0500

    got into api

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-let apikey = 'e00e070ff664e28f2e1b568199db890a';
-let otherkey = 'a89a2c364ea62c3a0e31da7f3191b3f3177e9ae0';
-let ts = '1';
+const apikey = 'e00e070ff664e28f2e1b568199db890a';
+const otherkey = 'a89a2c364ea62c3a0e31da7f3191b3f3177e9ae0';
+const ts = '1';
 //hash = '175541113e757d45c978347bd7877f22';
 
-let baseURL = 'http://gateway.marvel.com/v1/public/characters?';
+const urlBase = 'http://gateway.marvel.com/v1/public/characters?';
 
 function readyFunctions(){
         console.log('readyfunction ran');
-    getHash();
+    getName();
 }
 
 function getHash(){
@@ -22,7 +22,7 @@ function getHash(){
         
         console.log('hash: ' + hash);
 
-        getName();
+        return hash;
 }
 
 async function getName(){
@@ -36,7 +36,7 @@ async function getName(){
         let searchName = $('input[name="characterName"]').val(); 
             console.log('You are searching for: ' + searchName);
         
-            marvelAPI();
+            marvelAPI(searchName);
         
         });
 
@@ -54,8 +54,30 @@ function emptyDisplays(){
     $('#trending').empty();
 }
 
-function marvelAPI(){
+function marvelAPI(searchName){
     console.log('marvelAPI ran');
+
+    let hash = getHash();
+
+    let marvelSearch = urlBase + 'ts=' + ts + '&apikey=' + apikey + '&hash=' + hash + '&name=' + searchName;
+    //http://gateway.marvel.com/v1/public/characters?ts={{ts}}&apikey={{apikey}}&hash={{hash}}
+
+    console.log('fetch code: ' + marvelSearch);
+
+    fetch(marvelSearch)
+    .then(response => response.json())
+    .then(responseJSON => {
+        if (responseJSON.data.count === 0){
+            console.log('Count was: ' + responseJSON.data.count);
+            throw new Error(response.status);
+        }
+        else{
+            console.log('Count was: ' + responseJSON.data.count);
+            alert("Did it!");
+        }
+    })
+    .catch(err => alert("Couldn't that character"));
+
 }
 
 $(readyFunctions);
